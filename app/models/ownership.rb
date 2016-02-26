f3b4fed57331f39dc93354e46651099ff2d025b1
akienzo <ringstyle2@gmail.com>
2016-02-26 08:08:29 +0000

    kadai2have want0201602261708

--- app/models/ownership.rb
+++ app/models/ownership.rb
@@ -2,3 +2,5 @@ class Ownership < ActiveRecord::Base
   belongs_to :user, class_name: "User"
   belongs_to :item, class_name: "Item"
 end
+#ユーザーと、アイテムへの参照を持たせることで、「あるユーザーがあるアイテムを所有している」ということを表現する
+#Ownershipモデルは、usersテーブルとitemsテーブルの中間テーブルであるownershipsテーブルを使い、ownershipsテーブルには、user_idとitem_idが含まれる。
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,19 @@ class User < ActiveRecord::Base
   has_many :following_users, through: :following_relationships, source: :followed
   has_many :followed_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followed_users, through: :followed_relationships, source: :follower
-
+  
+  #has_many :items, through: :ownerships、Itemモデルではhas_many :users, through: :ownershipsとなっていて、
+  #:ownershipsを参照することで、それぞれ:itemsと:usersを取得している。
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
-
+  #has_many :ownershipsとは異なり、has_many :wantsではclass_name: "Want"を指定することで、ownershipsテーブルからtypeがWantであるものを取得します。
+  #このことにより、has_many :want_itemsで、wantしたアイテムの一覧を取得することができる。
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items , through: :wants, source: :item
+  #ownershipsテーブルからtypeがHaveであるものの一覧がhavesで、Haveしたアイテムの一覧がhave_itemsで取得できるようになります。
+  #source: :itemとしているのは、through: :havesで指定した参照先のクラスHaveに宣言されているbelongs_to :itemのitemを取得することを意味しています
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items , through: :haves, source: :item
 
   # 他のユーザーをフォローする
   def follow(other_user)
@@ -31,20 +40,43 @@ class User < ActiveRecord::Base
 
   ## TODO 実装
   def have(item)
+    haves.find_or_create_by(item_id: item.id)
   end
 
   def unhave(item)
+    haves.find_by(item_id: item.id).destroy
   end
 
   def have?(item)
+    have_items.include?(item)
   end
 
   def want(item)
+    # want.rbに紐づけるのかなと上記でhas_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroyとあるのでwantsかなと考えました
+    wants.find_or_create_by(item_id: item.id)
   end
 
   def unwant(item)
+    wants.find_by(item_id: item.id).destroy
   end
 
   def want?(item)
+    want_items.include?(item)
   end
 end
+
+# has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+# has_many :want_items , through: :wants, source: :item
+# ここで、has_many :ownershipsとは異なり、has_many :wantsではclass_name: "Want"を指定することで、ownershipsテーブルからtypeがWantであるものを取得します。
+# このことにより、has_many :want_itemsで、wantしたアイテムの一覧を取得することができます。
+ # 他のユーザーをフォローする 現在のユーザーのfollowing_relationshipsの中からフォローするユーザーのuser_idを含むものを探し、存在しない場合は、新しく作成します。
+    # def follow(other_user)
+    #   following_relationships.find_or_create_by(followed_id: other_user.id)
+    #   #数のパラメータと一致するものを1件取得し、存在する場合はそのオブジェクトを返し、存在しない場合は引数の内容で新しくオブジェクトを作成し、データベースに保存します。
+    # end
+  
+    # # フォローしているユーザーをアンフォローする following_relationshipsからフォローしているユーザーのuser_idが入っているものを探し、存在する場合は削除します
+    # def unfollow(other_user)
+    #   following_relationship = following_relationships.find_by(followed_id: other_user.id)
+    #   following_relationship.destroy if following_relationship
+    # end
